ff851198f97bca95c581c73c468ec0afb71d54fe
Ktasd Asffa <sapegina212@mail.ru>
2021-02-22 11:24:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp.Exception/MyOwnException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp.Exception
+{
+  class MyOwnException : System.Exception
+  {
+    public MyOwnException() : base("It's my exception")
+    {
+    }
+    public MyOwnException(string message) : base(message)
+    {
+    }
+  }
+}
---
+++ ConsoleApp.Exception/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp.Exception
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int i = 101;
+      try
+      {
+        //var a = 5;
+        //var b = a / 0;
+        //Console.WriteLine(b);
+
+        //throw new DivideByZeroException("gasgagwgtqwgsgagwgqwg");
+        throw new MyOwnException();
+      }
+      catch (MyOwnException ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+      catch (System.Exception ex) when (i == 10)
+      {
+
+        Console.WriteLine(ex.Message + " (i == 10)");
+      }
+      catch (System.Exception ex)
+      {
+
+        Console.WriteLine(ex.Message);
+      }
+      finally
+      {
+        Console.WriteLine("Done");
+      }
+      Console.ReadLine();
+    }
+  }
+}
